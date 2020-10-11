e8975df7bfeb3b0f6cfe867302d3b1e4b6aefd84
William tr <tr.william@gmail.com>
2020-10-11 18:11:28 +0200

    build sidebar and chatbar update

--- src/chat.js
+++ src/chat.js
@@ -1,10 +1,36 @@
+import { AttachFile, MoreVert, SearchOutlined } from '@material-ui/icons';
 import React from  'react';
-import "./chat.css"
+import "./chat.css";
+import { IconButton, Avatar } from '@material-ui/core';
+
 
 function Chat() {
     return (
         <div className="chat">
-            <h1>Iam a chat</h1>
+            <div className="chat__header">
+                <Avatar/>
+
+
+
+                <div className="chat__headerInfo">
+                    <h3>Room name</h3>
+                    <p>Last seen at...</p>
+                    </div>
+
+                <div className="chat__headerInfoRight"></div>
+
+
+                    <IconButton>
+                    <SearchOutlined/>
+                    </IconButton>
+                    <IconButton>
+                    <AttachFile/>
+                    </IconButton>
+                    <IconButton>
+                    <MoreVert/>
+                    </IconButton>
+                    </div>
+
         </div>
     )
 }
--- src/sidebar.js
+++ src/sidebar.js
@@ -4,26 +4,40 @@ import DonutLargeIcon from '@material-ui/icons/DonutLarge';
 import { IconButton, Avatar } from '@material-ui/core';
 import ChatIcon from '@material-ui/icons/Chat';
 import MoreVertIcon from '@material-ui/icons/MoreVert';
+import {SearchOutlined} from '@material-ui/icons';
+import SidebarChat from './SidebarChat'
 
 
 function Sidebar() {
     return (
         <div className ="sidebar">
-        <div className = "sidebar__header">
-            <Avatar/>
-            <div className="sidebar__headerRight">
-                <IconButton>
-                 <DonutLargeIcon/>
-                </IconButton>
-                <IconButton>
-                 <ChatIcon/>
-                </IconButton>
-                <IconButton>
-                 <MoreVertIcon/>
-                </IconButton>
+            <div className = "sidebar__header">
+                <Avatar src ="https://user-images.githubusercontent.com/64305892/95625393-b725e780-0a78-11eb-936b-e035be8d12d7.jpg"/>
+                <div className="sidebar__headerRight">
+                    <IconButton>
+                    <DonutLargeIcon/>
+                    </IconButton>
+                    <IconButton>
+                    <ChatIcon/>
+                    </IconButton>
+                    <IconButton>
+                    <MoreVertIcon/>
+                    </IconButton>
+                </div>
             </div>
-        </div>
 
+            <div className = "sidebar__search">
+                <div className="sidebar__searchContainer">
+                    <SearchOutlined/>
+                    <input placeholder = "Search or start new chat" type = "text" />
+                </div>
+            </div>
+            <div className="sidebar__chats">
+                    <SidebarChat/>
+                    <SidebarChat/>
+                    <SidebarChat/>
+
+            </div>
         </div>
     )
 }
