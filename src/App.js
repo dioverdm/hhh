d473d3cc404e31817dfcbe83f33b119e4a26785a
William tr <tr.william@gmail.com>
2020-10-19 01:10:32 +0200

    last dev between front to back sync

--- src/App.js
+++ src/App.js
@@ -1,13 +1,23 @@
-import React, {useEffect} from 'react';
+import React, {useEffect, useState} from 'react';
 import './App.css';
 import Sidebar from './sidebar';
 import Chat from './chat';
 import Pusher from "pusher-js";
+import axios from "./axios"
 
 
 // 2 composants un menu avec les conversation et une fenêtre de chat
 
 function App() {
+
+  const [messages, setMessages] = useState([]);
+
+  useEffect(() => {
+    axios.get('/messages/sync').then((response) => {
+        setMessages(response.data);
+      });
+  }, []);
+
 
   useEffect(() => {
     const pusher = new Pusher('53c801d90ada0b8103b6', {
@@ -19,6 +29,9 @@ function App() {
       alert(JSON.stringify(data));
     });
   }, []);
+
+
+  console.log(messages);
 
   return (
     <div className="app">
